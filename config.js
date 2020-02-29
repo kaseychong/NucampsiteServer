258bc73701a29fbdc985a897937251d03ad96232
Kasey Chong <kchong1989@gmail.com>
2020-02-28 22:27:07 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '196008038320608',
+        clientSecret: '5a9d6ac748174c00e0b6921c4308b34d'
+    }
 }
